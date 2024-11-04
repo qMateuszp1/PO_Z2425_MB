daf489a80b1d795d72c3d3f04020f48bf2aa9846
qMateuszp1 <matboch6@wp.pl>
2024-11-04 19:55:29 +0100

    circle.java

--- src/circle.java
+++ src/circle.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+public class Circle extends Shape {
+    private Point center;
+    private double diameter;
 
-public class circle {
+    public Circle(Point center, double diameter) {
+        this.center = center;
+        this.diameter = diameter;
+    }
+    @Override
+    public double area(){
+        double radius = diameter / 2;
+        return Math.PI * radius * radius;
+    }
+    @Override
+    public double circumference() {
+        return Math.PI * diameter;
+    }
 }
