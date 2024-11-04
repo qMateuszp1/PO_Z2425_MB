2d44f934f9bedee3076e7b854ada54c062421a15
qMateuszp1 <matboch6@wp.pl>
2024-11-04 20:00:05 +0100

    Update triangle.java

--- src/triangle.java
+++ src/triangle.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+public class Triangle extends Shape{
+    private double height;
+    private double base;
+    public Triangle(double base, double height){
+        this.base = base;
+        this.height = height;
+    }
 
-public class triangle {
+    @Override
+    public double area(){
+        return 0.5 * base * height;
+
+    }
+    @Override
+    public double circumference(){
+        return 0.0;
+    }
 }
