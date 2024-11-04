a5ef671b8531fa850c634e8015001effe63bae4d
qMateuszp1 <matboch6@wp.pl>
2024-11-04 19:54:38 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Circle circle = new Circle(new Point(0, 0),10 );
+        Square square = new Square(5);
+        Triangle triangle = new Triangle(6, 8);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Calculator calc = new Calculator();
+        System.out.println("Suma powierzchni koła i kwadratu: " + calc.calculateAreaSum(circle, square));
+        System.out.println("Suma powierzchni trojkata i kwadratu: " + calc.calculateAreaSum(triangle, square));
+        System.out.println("Suma powierzchni koła i trojkata: " + calc.calculateAreaSum(circle, triangle));
     }
 }
