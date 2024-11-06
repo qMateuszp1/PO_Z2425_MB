6b9aab97f02b86a95460e4b41a3a601e8b154f22
qMateuszp1 <matboch6@wp.pl>
2024-11-06 21:34:58 +0100

    Update calculator.java

--- src/calculator.java
+++ src/calculator.java
@@ -1,5 +1,19 @@
 public class Calculator {
-    public double calculateAreaSum(Shape shape1, Shape shape2) {
-        return shape1.area() + shape2.area();
-    }
+        // odleglosc pomiedzy punktami
+        public double distance(Point a, Point b) {
+            double dx = b.getX() - a.getX();
+            double dy = b.getY() - a.getY();
+            return Math.sqrt(dx * dx + dy * dy);
+        }
+
+        // odleglosc x
+        public double distanceX(Point a, Point b) {
+            return Math.abs(b.getX() - a.getX());
+        }
+
+        // odleglosc y
+        public double distanceY(Point a, Point b) {
+            return Math.abs(b.getY() - a.getY());
+        }
+
 }
