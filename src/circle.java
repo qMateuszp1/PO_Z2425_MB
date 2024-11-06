ab991a28fffd329d745765b1baa6d742b0714cff
qMateuszp1 <matboch6@wp.pl>
2024-11-06 21:35:26 +0100

    Update circle.java

--- src/circle.java
+++ src/circle.java
@@ -1,17 +1,20 @@
-public class Circle extends Shape {
+public class Circle {
     private Point center;
     private double diameter;
+
 
     public Circle(Point center, double diameter) {
         this.center = center;
         this.diameter = diameter;
     }
-    @Override
-    public double area(){
+
+    // pole kola
+    public double area() {
         double radius = diameter / 2;
         return Math.PI * radius * radius;
     }
-    @Override
+
+    // oobwod kola
     public double circumference() {
         return Math.PI * diameter;
     }
