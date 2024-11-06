6797a1bd4ef23cd3415ffc394abeb52dbe19a8e2
qMateuszp1 <matboch6@wp.pl>
2024-11-06 21:34:29 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        Circle circle = new Circle(new Point(0, 0),10 );
-        Square square = new Square(5);
-        Triangle triangle = new Triangle(6, 8);
+        // punkty
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(3, 4);
 
+        // kalkulator i obliczenia
         Calculator calc = new Calculator();
-        System.out.println("Suma powierzchni koła i kwadratu: " + calc.calculateAreaSum(circle, square));
-        System.out.println("Suma powierzchni trojkata i kwadratu: " + calc.calculateAreaSum(triangle, square));
-        System.out.println("Suma powierzchni koła i trojkata: " + calc.calculateAreaSum(circle, triangle));
+        System.out.println("Odległość między p1 i p2: " + calc.distance(p1, p2));
+        System.out.println("Odległość w osi X: " + calc.distanceX(p1, p2));
+        System.out.println("Odległość w osi Y: " + calc.distanceY(p1, p2));
+
+        // okrag i obliczenia
+        Circle circle = new Circle(p1, 10);
+        System.out.println("Pole koła: " + circle.area());
+        System.out.println("Obwód koła: " + circle.circumference());
     }
 }
