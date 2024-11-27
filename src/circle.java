b7de826b3b9af4d94a00acb7ac6763bbb918f2e6
qMateuszp1 <matboch6@wp.pl>
2024-11-27 18:13:53 +0100

    circle.java

--- src/circle.java
+++ src/circle.java
@@ -2,15 +2,28 @@ public class Circle extends Shape {
     private Point center;
     private double diameter;
 
-    public Circle(Point center, double diameter) {
+    public Circle(Point center, double diameter) throws BadShapeException {
+        if (diameter <= 0) {
+            try {
+                throw new BadShapeException("średnica musi byc wieksza od 0.");
+            }
+            catch (BadShapeException e) {
+                System.out.println(e);
+            }
+            finally {
+                System.out.println("Zakonczono obsluge bledow");
+            }
+            }
         this.center = center;
         this.diameter = diameter;
     }
+
     @Override
-    public double area(){
+    public double area() {
         double radius = diameter / 2;
         return Math.PI * radius * radius;
     }
+
     @Override
     public double circumference() {
         return Math.PI * diameter;
