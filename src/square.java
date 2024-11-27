be1357a9678530cd5a9d1ae2114c0658e6cc1568
qMateuszp1 <matboch6@wp.pl>
2024-11-27 18:14:18 +0100

    square.java

--- src/square.java
+++ src/square.java
@@ -1,6 +1,19 @@
 public class Square extends Shape{
     private double side;
     public Square(double side) {
+        if (side <= 0){
+            try {
+                throw new
+                        BadShapeException("side must be greater than zero.");
+            }
+            catch (BadShapeException e) {
+                System.out.println(e);
+            }
+            finally {
+                System.out.println("Zakonczono obsluge bledow");
+            }
+            }
+
         this.side = side;
 
     }
